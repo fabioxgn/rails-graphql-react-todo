6addfa5a93128aec73d8b87a1152ce66d547887d
Fábio Gomes <fabioxgn@gmail.com>
2021-03-27 12:07:19 -0300

    Add todos type

---
+++ app/graphql/types/todo_item_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class TodoItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :description, String, null: false
+    field :completed, Boolean, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
