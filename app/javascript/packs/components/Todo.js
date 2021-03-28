5c8fe6a774ffe2d4dbd4000b290ddd4420b1fd1e
Fábio Gomes <fabioxgn@gmail.com>
2021-03-27 23:42:11 -0300

    Allow deleting todos

--- app/javascript/packs/components/Todo.js
+++ app/javascript/packs/components/Todo.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useMutation } from 'react-apollo'
-import { UpdateTodo } from './operations.graphql'
+import { UpdateTodo, DestroyTodo, TodosQuery } from './operations.graphql'
 
 function Todo (props) {
   const todo = props.todo
 
   const [updateTodo] = useMutation(UpdateTodo)
+  const [destroyTodo] = useMutation(DestroyTodo, {
+    update: (cache, { data: { destroyTodo } }) => {
+      const destroyed = destroyTodo
+      if (destroyed) {
+        const currentTodos = cache.readQuery({ query: TodosQuery })
+        cache.writeQuery({
+          query: TodosQuery,
+          data: { todos: currentTodos.todos.filter(todo => todo.id != destroyed.id) }
+        })
+      }
+    }
+  })
   return(
     <li>
       <input className='mr-2' type='checkbox' checked={todo.completed} id={`todo_${todo.id}`}
@@ -14,7 +26,13 @@ function Todo (props) {
         }}
       />
       {todo.description}
-      <i className="bi-trash" />
+      <a href="#" className='text-reset'>
+        <i className="bi-trash" id={`todo_${todo.id}`}
+          onClick={e => {
+            destroyTodo({ variables: { id: e.target.id.replace(/^todo_/, '') }})
+          }}
+        />
+      </a>
     </li>
   )
 }
