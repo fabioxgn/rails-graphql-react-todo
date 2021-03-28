b0bb8c4ffaa0c21d3b73fa63b0e4b80b15b2b06f
Fábio Gomes <fabioxgn@gmail.com>
2021-03-27 22:52:03 -0300

    Allow completing todos

--- app/javascript/packs/components/Todo.js
+++ app/javascript/packs/components/Todo.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useMutation } from 'react-apollo'
+import { UpdateTodo } from './operations.graphql'
 
 function Todo (props) {
   const todo = props.todo
-  return <li>{todo.description}</li>
+
+  const [updateTodo] = useMutation(UpdateTodo)
+  return(
+    <li>
+      <input className='mr-2' type='checkbox' checked={todo.completed} id={`todo_${todo.id}`}
+        onChange={e => {
+          updateTodo({ variables: { id: e.target.id.replace(/^todo_/, ''), completed: e.target.checked } })
+        }}
+      />
+      {todo.description}
+    </li>
+  )
 }
 
 export default Todo
